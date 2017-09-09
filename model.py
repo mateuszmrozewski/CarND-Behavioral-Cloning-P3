b644c56d9b58b6d11ad1d621f7bb5b8aaec6d756
Mateusz Mrozewski <mateusz@tlt.local>
2017-09-09 21:13:09 +0400

    Next few tries, current one with nvidia architecture

--- model.py
+++ model.py
@@ -16,10 +16,10 @@ for line in lines:
     current_path = 'a/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
-    images.append(cv2.flip(image, 1))
+    #images.append(cv2.flip(image, 1))
     measurement = float(line[3])
     measurements.append(measurement)
-    measurements.append(measurement * -1.0)
+    #measurements.append(measurement * -1.0)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
@@ -36,18 +36,17 @@ model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((50,20), (0,0))))
 
 
-model.add(Convolution2D(20, 5, 5, border_mode="same", activation="relu"))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
-model.add(Convolution2D(50, 5, 5, border_mode="same"))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-
+model.add(Convolution2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
+model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
+model.add(Convolution2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
 model.add(Flatten())
-model.add(Dense(500))
-model.add(Activation("relu"))
-
-model.add(Dense(1))
+model.add(Dense(1164, activation='relu'))
+model.add(Dense(100, activation='relu'))
+model.add(Dense(50, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1, activation='tanh'))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
