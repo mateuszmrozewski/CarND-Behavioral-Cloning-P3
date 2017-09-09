00627da25fe3c6e67b4c2a94a321f1f1cb59691e
Mateusz Mrozewski <mateusz@tlt.local>
2017-09-09 14:16:11 +0400

    Results of first runs on LeNet wihout tuning

--- run1/model.py
+++ run1/model.py
@@ -33,7 +33,6 @@ from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPooling2D, Ac
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
-model.add(Cropping2D(cropping=((50,20), (0,0))))
 
 
 model.add(Convolution2D(20, 5, 5, border_mode="same", activation="relu"))
